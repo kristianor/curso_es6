b75fccc7fd5413d71a6ece407c46b242892980cf
kristianor <kristianor@hotmail.com>
2019-09-28 13:19:03 -0300

    Uso de classes ES6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-class Teste {
-    metodo() {
 
+class List {
+    constructor() {
+        this.data = [];
+    }
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List {
+
+    constructor() {
+        super();
+        this.usuario = 'Kristiano';
+    }
+
+    monstraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novoTodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.monstraUsuario();
